9e07672404e37d7ad9c3a3ff62626b239c12f6b1
mintyway0112 <mintyway0112@gmail.com>
2025-03-28 21:57:34 +0900

    init

---
+++ LunaDX11/LunaDX11.cpp
@@ -0,0 +1,12 @@
+#include <Windows.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	return 0;
+}
