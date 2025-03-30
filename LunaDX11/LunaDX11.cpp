90152c5fbbd2ebf98b5f6a08a432cc035fa92557
mintyway0112 <mintyway0112@gmail.com>
2025-03-31 04:11:20 +0900

    디버깅용 콘솔 추가

--- LunaDX11/LunaDX11.cpp
+++ LunaDX11/LunaDX11.cpp
@@ -1,9 +1,21 @@
 #include <Windows.h>
 
 #include "Core/Engine.h"
+#include <iostream>
+
+void CreateDebugConsole()
+{
+    FILE* dummy; // 매개변수 채우기 용 더미 포인터
+    AllocConsole(); // 콘솔 할당
+    (void)freopen_s(&dummy, "CONOUT$", "w", stdout); // 표준 출력 재지정
+    (void)freopen_s(&dummy, "CONOUT$", "w", stderr); // 표준 에러 재지정
+    (void)freopen_s(&dummy, "CONIN$", "r", stdin); // 표준 입력 재지정
+}
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
+    CreateDebugConsole();
+
     // 게임 엔진 초기화
     if (!Engine::Get()->Init(hInstance, [](HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) { return Engine::Get()->HandleMessage(hWnd, message, wParam, lParam); }))
     {
