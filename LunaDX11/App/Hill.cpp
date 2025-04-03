29d55478452d708fb64464e31040cee9e905fdb4
mintyway0112 <mintyway0112@gmail.com>
2025-04-04 01:57:06 +0900

    언덕 예제 완성

--- LunaDX11/App/Hill.cpp
+++ LunaDX11/App/Hill.cpp
@@ -1,5 +1,6 @@
 #include "Hill.h"
 
+#include "../Core/GeometryGenerator.h"
 #include "../Core/Utility.h"
 
 #include <algorithm>
@@ -10,45 +11,6 @@ HillApp::HillApp()
     XMStoreFloat4x4(&worldMatrix, identityMatrix);
     XMStoreFloat4x4(&viewMatrix, identityMatrix);
     XMStoreFloat4x4(&projectionMatrix, identityMatrix);
-
-    boxVertices =
-    {
-        Vertex{XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT3(Colors::White)},
-        Vertex{XMFLOAT3(-1.0f, +1.0f, -1.0f), XMFLOAT3(Colors::Black)},
-        Vertex{XMFLOAT3(+1.0f, +1.0f, -1.0f), XMFLOAT3(Colors::Red)},
-        Vertex{XMFLOAT3(+1.0f, -1.0f, -1.0f), XMFLOAT3(Colors::Green)},
-        Vertex{XMFLOAT3(-1.0f, -1.0f, +1.0f), XMFLOAT3(Colors::Blue)},
-        Vertex{XMFLOAT3(-1.0f, +1.0f, +1.0f), XMFLOAT3(Colors::Yellow)},
-        Vertex{XMFLOAT3(+1.0f, +1.0f, +1.0f), XMFLOAT3(Colors::Cyan)},
-        Vertex{XMFLOAT3(+1.0f, -1.0f, +1.0f), XMFLOAT3(Colors::Magenta)}
-    };
-
-    boxIndices =
-    {
-        // 앞면
-        0, 1, 2,
-        0, 2, 3,
-
-        // 뒷면
-        7, 6, 5,
-        7, 5, 4,
-
-        // 좌측면
-        4, 5, 1,
-        4, 1, 0,
-
-        // 우측면
-        3, 2, 6,
-        3, 6, 7,
-
-        // 윗면
-        1, 5, 6,
-        1, 6, 2,
-
-        // 밑면
-        4, 0, 3,
-        4, 3, 7
-    };
 }
 
 bool HillApp::Init(HINSTANCE inInstanceHandle)
@@ -100,10 +62,10 @@ void HillApp::OnMouseMove(WPARAM buttonState, int x, int y)
     }
     else if (buttonState & MK_RBUTTON)
     {
-        constexpr float zoomSpeed = 0.01f; // 1픽셀당 0.01만큼 근접 or 멀어짐
+        constexpr float zoomSpeed = 0.2f; // 1픽셀당 0.2만큼 근접 or 멀어짐
         const float dx = zoomSpeed * static_cast<float>(currentMousePosition.x - lastMousePosition.x);
 
-        radius = std::clamp(radius - (dx), 3.0f, 15.0f);
+        radius = std::clamp(radius - (dx), 100.0f, 600.0f);
     }
 
     lastMousePosition = currentMousePosition;
@@ -123,7 +85,7 @@ void HillApp::Render()
 {
     Super::Render();
 
-    if (!immediateContext || !renderTargetView || !depthStencilView || !inputLayout || !boxVertexBuffer || !boxIndexBuffer || !constantBuffer || !vertexShader || !pixelShader)
+    if (!immediateContext || !renderTargetView || !depthStencilView || !inputLayout || !vertexBuffer || !indexBuffer || !constantBuffer || !vertexShader || !pixelShader)
     {
         return;
     }
@@ -138,8 +100,8 @@ void HillApp::Render()
 
     constexpr UINT stride = sizeof(Vertex);
     constexpr UINT offset = 0;
-    immediateContext->IASetVertexBuffers(0, 1, boxVertexBuffer.GetAddressOf(), &stride, &offset);
-    immediateContext->IASetIndexBuffer(boxIndexBuffer.Get(), DXGI_FORMAT_R32_UINT, 0);
+    immediateContext->IASetVertexBuffers(0, 1, vertexBuffer.GetAddressOf(), &stride, &offset);
+    immediateContext->IASetIndexBuffer(indexBuffer.Get(), DXGI_FORMAT_R32_UINT, 0);
 
     // WVP 행렬
     const XMMATRIX wvpMatrix = XMLoadFloat4x4(&worldMatrix) * XMLoadFloat4x4(&viewMatrix) * XMLoadFloat4x4(&projectionMatrix);
@@ -152,22 +114,71 @@ void HillApp::Render()
     immediateContext->VSSetShader(vertexShader.Get(), nullptr, 0);
     immediateContext->PSSetShader(pixelShader.Get(), nullptr, 0);
 
-    immediateContext->DrawIndexed(36, 0, 0);
+    /*Draw와 DrawIndexed의 차이
+     * Draw
+     * 버텍스버퍼 = { v0, v1, v2, v0, v2, v3}
+     *
+     * DrawIndexed
+     * 버텍스버퍼 = { v0, v1, v2, v3}
+     * 인덱스버퍼 = { 0, 1, 2, 0, 2, 3}
+     *
+     * Draw는 인덱스 버퍼없이 버텍스 버퍼만 사용하는데 버텍스를 중복정의하면서 그려야한다. (버텍스는 데이터 크기가 거대해 중복정의해서는 안된다.)
+     * DrawIndexed는 중복되지 않는 버텍스 버퍼와 단순 순서정보만 나열된 인덱스 버퍼를 통해서 그린다.
+     *
+     * 따라서 99%의 경우 DrawIndexed를 사용한다.
+     */
+    // 셰이더 호출
+    immediateContext->DrawIndexed(indexCount, 0, 0);
 
     swapChain->Present(0, 0);
 }
 
 void HillApp::CreateGeometryBuffers()
 {
+    GeometryGenerator::MeshData grid;
+    GeometryGenerator::CreateGrid(160.0f, 160.0f, 50, 50, grid);
+
+    std::vector<Vertex> vertices(grid.vertices.size());
+    for (size_t i = 0; i < grid.vertices.size(); ++i)
+    {
+        XMFLOAT3 position = grid.vertices[i].position;
+        position.y = GetHeight(position.x, position.z);
+        vertices[i].pos = position;
+
+        if (position.y < -10.0f)
+        {
+            vertices[i].color = XMFLOAT4(1.0f, 0.96f, 0.62f, 1.0f);
+        }
+        else if (position.y < 5.0f)
+        {
+            vertices[i].color = XMFLOAT4(0.48f, 0.77f, 0.46f, 1.0f);
+        }
+        else if (position.y < 12.0f)
+        {
+            vertices[i].color = XMFLOAT4(0.1f, 0.48f, 0.19f, 1.0f);
+        }
+        else if (position.y < 20.0f)
+        {
+            vertices[i].color = XMFLOAT4(0.45f, 0.39f, 0.34f, 1.0f);
+        }
+        else
+        {
+            vertices[i].color = XMFLOAT4(0.48f, 0.96f, 0.62f, 1.0f);
+        }
+    }
+
     // 버텍스 버퍼 생성
-    const CD3D11_BUFFER_DESC vertexBufferDesc(static_cast<UINT>(sizeof(Vertex) * boxVertices.size()), D3D11_BIND_VERTEX_BUFFER, D3D11_USAGE_IMMUTABLE);
-    const D3D11_SUBRESOURCE_DATA vertexInitData{boxVertices.data()};
-    CHECK_HR(device->CreateBuffer(&vertexBufferDesc, &vertexInitData, &boxVertexBuffer), L"버텍스 버퍼 생성에 실패했습니다.");
+    const CD3D11_BUFFER_DESC vertexBufferDesc(static_cast<UINT>(sizeof(Vertex) * vertices.size()), D3D11_BIND_VERTEX_BUFFER, D3D11_USAGE_IMMUTABLE);
+    const D3D11_SUBRESOURCE_DATA vertexInitData{vertices.data()};
+    CHECK_HR(device->CreateBuffer(&vertexBufferDesc, &vertexInitData, &vertexBuffer), L"버텍스 버퍼 생성에 실패했습니다.");
 
     // 인덱스 버퍼 생성
-    const CD3D11_BUFFER_DESC indexBufferDesc(static_cast<UINT>(sizeof(UINT) * boxIndices.size()), D3D11_BIND_INDEX_BUFFER, D3D11_USAGE_IMMUTABLE);
-    const D3D11_SUBRESOURCE_DATA indexInitData{boxIndices.data()};
-    CHECK_HR(device->CreateBuffer(&indexBufferDesc, &indexInitData, &boxIndexBuffer), L"인덱스 버퍼 생성에 실패했습니다.");
+    const CD3D11_BUFFER_DESC indexBufferDesc(static_cast<UINT>(sizeof(UINT) * grid.indices.size()), D3D11_BIND_INDEX_BUFFER, D3D11_USAGE_IMMUTABLE);
+    const D3D11_SUBRESOURCE_DATA indexInitData{grid.indices.data()};
+    CHECK_HR(device->CreateBuffer(&indexBufferDesc, &indexInitData, &indexBuffer), L"인덱스 버퍼 생성에 실패했습니다.");
+
+    // 인덱스 카운트 업데이트
+    indexCount = static_cast<UINT>(grid.indices.size());
 }
 
 void HillApp::CreateShaders()
--- LunaDX11/App/Hill.h
+++ LunaDX11/App/Hill.h
@@ -14,6 +14,8 @@ class HillApp : public BaseEngine
 public:
     ~HillApp() override = default;
 
+    static float GetHeight(float x, float z) { return 0.3f * (z * std::sin(0.1f * x) + x * std::cos(0.1f * z)); }
+
     bool Init(HINSTANCE inInstanceHandle) override;
     void OnResize() override;
     void OnMouseDown(WPARAM buttonState, int x, int y) override;
@@ -30,8 +32,9 @@ private:
     void CreateGeometryBuffers();
     void CreateShaders();
 
-    ComPtr<ID3D11Buffer> boxVertexBuffer;
-    ComPtr<ID3D11Buffer> boxIndexBuffer;
+    ComPtr<ID3D11Buffer> vertexBuffer;
+    ComPtr<ID3D11Buffer> indexBuffer;
+    UINT indexCount = 0;
 
     ComPtr<ID3D11VertexShader> vertexShader;
     ComPtr<ID3D11PixelShader> pixelShader;
@@ -43,12 +46,9 @@ private:
     XMFLOAT4X4 viewMatrix;
     XMFLOAT4X4 projectionMatrix;
 
-    float radius = 5.0f;
+    float radius = 200.0f;
     float theta = -XM_PIDIV4;
     float phi = 0.25f * XM_PI;
 
     POINT lastMousePosition{};
-
-    std::vector<Vertex> boxVertices;
-    std::vector<UINT> boxIndices;
 };
