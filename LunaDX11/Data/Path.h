a575b9eb8058dd0448075c4ae84e1c1e3418fa9c
mintyway0112 <mintyway0112@gmail.com>
2025-04-13 02:53:59 +0900

    편의성을 위한 경로 전역 변수화

---
+++ LunaDX11/Data/Path.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace ShaderPath
+{
+    constexpr const wchar_t* Chapter6VertexShader = L"../LunaDX11/Shader/Vertex/Box_vs.hlsl";
+    constexpr const wchar_t* Chapter6PixelShader = L"../LunaDX11/Shader/Pixel/Box_ps.hlsl";
+}
+
+namespace MeshPath
+{
+    constexpr const char* SkullPath = "../LunaDX11/Models/skull.txt";
+}
