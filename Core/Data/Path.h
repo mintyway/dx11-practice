9a5ba4f0455e84f764ec29a8f9a0a1c515713c45
mintyway0112 <mintyway0112@gmail.com>
2025-04-13 10:50:57 +0900

    모듈화를 위해 프로젝트명 core로 변경

---
+++ Core/Data/Path.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace ShaderPath
+{
+    constexpr const wchar_t* Chapter6VertexShader = L"../Core/Shader/Vertex/Box_vs.hlsl";
+    constexpr const wchar_t* Chapter6PixelShader = L"../Core/Shader/Pixel/Box_ps.hlsl";
+}
+
+namespace MeshPath
+{
+    constexpr const char* SkullPath = "../Core/Models/skull.txt";
+}
