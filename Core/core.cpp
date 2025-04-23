2afb7938cf3ec5113f98e011d1ac7ba9de1f9bbb
mintyway0112 <mintyway0112@gmail.com>
2025-04-23 14:37:06 +0900

    core 수정

--- Core/core.cpp
+++ Core/core.cpp
@@ -1,21 +1,18 @@
-#include <Windows.h>
 
-#include "App/Chapter6/WavesApp.h"
-
-
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
-{
-    // 엔진 등록
-    // BaseEngine::Register<AppClass>();
-
-    // 게임 엔진 초기화
-    // if (!BaseEngine::Get()->Init(hInstance))
-    // {
-    //     return 0;
-    // }
-
-    // 엔진 실행
-    // BaseEngine::Get()->Run();
-
-    return 0;
-}
+// 사용 방법 예시
+// int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+// {
+//     엔진 등록
+//     BaseEngine::Register<AppClass>();
+//
+//     게임 엔진 초기화
+//     if (!BaseEngine::Get()->Init(hInstance))
+//     {
+//         return 0;
+//     }
+//
+//     엔진 실행
+//     BaseEngine::Get()->Run();
+//
+//     return 0;
+// }
